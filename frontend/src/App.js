0c816416ead2664e2e13bb1d911bcfd86458e35b
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-07-29 17:26:39 -0500

    working on initial styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import Landing from "./components/Landing"; 
+import styled from "styled-components"; 
 
 function App() {
+
+  const StyleDiv = styled.div`
+  background: #3A3A3A; 
+  `
   return (
-    <div className="App">
+    <StyleDiv className="App">
       <Landing />
-    </div>
+    </StyleDiv>
   );
 }
 
