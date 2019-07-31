267230b8378c3f6e15c9c5740218f5ed99cb7c52
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-07-31 16:28:03 -0500

    Uploading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import MainForm from './components/MainForm.js';
+import AppCard from './components/AppCard'
+
 
 function App() {
+  const [users, setUsers] = useState([
+    {id: 1, email: "v.a@gmail.com", password: "PasswordSun", application: "Twitter"},
+    {id: 1, email: "a.b@gmail.com", password: "PasswordMoon", application: "Netflix"}
+  ]);
+
+  const addPerson = person => {
+    setUsers([...users, person])
+  };
+
   return (
     <div className="App">
-      <p>Main page</p>
+      <MainForm submitUser={addPerson} />
+      {users.map(person => <AppCard person={person} />)}
     </div>
   );
 }
