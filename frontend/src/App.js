0536692eb78c3075ca2ed5d7bcf4413f6a28c02a
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-08-01 16:44:00 -0500

    merging for Obaida

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,24 +7,27 @@ import DataToPass from './components/DataToPassToForm';
 
 // import DataToPass from "./Components/DataToPassToForm.js";
 import Login from "./components/Login";
-import Signup from "./Components/Signup.js";  
+import Signup from "./components/Signup.js";  
 
 
 
 function App() {
-  const [users, setUsers] = useState([
-    {id: 1, email: "v.a@gmail.com", password: "PasswordSun", application: "Twitter"},
-    {id: 2, email: "a.b@gmail.com", password: "PasswordMoon", application: "Netflix"}
-  ]);
+  // const [users, setUsers] = useState([
+  //   {id: 1, email: "v.a@gmail.com", password: "PasswordSun", application: "Twitter"},
+  //   {id: 2, email: "a.b@gmail.com", password: "PasswordMoon", application: "Netflix"}
+  // ]);
 
-  const addAppCard = person => {
-    setUsers([...users, person])
-  };
+  // const addAppCard = person => {
+  //   setUsers([...users, person])
+  // };
 
   return (
     <div className="App">
-      <MainForm submitUser={addAppCard} /> 
-      {users.map(person => <AppCard person={person} />)}
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/home" component={DataToPass} />
+      {/* <MainForm submitUser={addAppCard} /> 
+      {users.map(person => <AppCard person={person} />)} */}
     </div>
   );
 
